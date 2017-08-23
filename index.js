f7c7fd237b9424e66f5eff91cd33e8709357f865
Inzamam Malik <malikasinger1@gmail.com>
2017-08-23 19:49:53 +0500

    a mile stone, not yet done

--- index.js
+++ index.js
@@ -6,32 +6,90 @@ var app = express();
 
 app.use(bodyParser.json())
 
-
+const skillId = "amzn1.ask.skill.063173ce-f192-4a0d-a3e0-8ba3c9e0d1e2"
+function buildResponse(sessionAttributes, speechText, cardObj, repromptObj) {
+    var response = {
+        "version": "1.0",
+        "sessionAttributes": sessionAttributes || {},
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": speechText || "default speech out"
+            },
+            "card": cardObj || {
+                "type": "Simple",
+                "title": "Simple title",
+                "content": speechText
+            },
+            "reprompt": repromptObj || {},
+            "shouldEndSession": false
+        }
+    }
+    return response;
+}
 app.use('/alexa-webhook', function (req, res, next) {//alexa webhook
 
-    console.log("_____________________________________________________");
     console.log("request: ", req.body);
 
     if (req.body.request.type === "LaunchRequest") {
+        console.log("Launch request___________________________________________________________________________________________");
+        var speechOutput = "Hi I am hotel booking bot, i can book hotel for you"
+        var data = buildResponse(req.body.session.attributes, speechOutput)
 
-        console.log("onLaunch requestId=" + req.body.request.requestId + ", sessionId=" + req.body.session.sessionId);
-        var cardTitle = "Hello, World!"
-        var speechOutput = "Hi! I'm hotel booking bot, i can book hotel for you"
-        var data = buildResponse(req.body.session.attributes, buildSpeechletResponse(cardTitle, speechOutput, "", true))
-        console.log("================================================================================================");
+        console.log("Launch response================================================================================================");
         console.log("Response: ", data);
         res.send(data);
 
     } else if (req.body.request.type === "IntentRequest") {
+        console.log("intent request_____________________________________________________");
+        console.log("intent name: ", req.body.request.intent.name);
 
-        onIntent(req.body.request, req.body.session,
-            function callback(sessionAttributes, speechletResponse) {
+        switch (req.body.request.intent.name) {
+            case 'booking':
 
-                res.send(buildResponse(sessionAttributes, speechletResponse));
-            });
+                console.log("booking slots ", req.body.request.intent.slots);
+
+                var slotsArr = Object.keys(req.body.request.intent.slots);
+
+                for (let i = 0; i < slotsArr.length; i++) {
+
+                    let item = slotsArr[i];
+                    console.log("slot item: ", item);
+                    if (!item.value) {
+
+                        var reprompt = {
+                            "outputSpeech": {
+                                "type": "PlainText",
+                                "text": "this is re prompt text"
+                            }
+                        }
+                    }
+                }
+
+
+
+                var card = {
+                    "type": "Standard",
+                    "title": "card title",
+                    "content": "this is content of card",
+                    "text": "this is some text of card",
+                    "image": {
+                        "smallImageUrl": "https://assets-cdn.github.com/images/modules/open_graph/github-mark.png",
+                        "largeImageUrl": "https://bgcdn.s3.amazonaws.com/wp-content/uploads/2013/04/5-2-God-is-in-Nature.jpg"
+                    }
+                }
+
+                var resData = buildResponse(req.body.session.attributes, "this is speech out", card)
+                console.log("intent Response ====> ", resData);
+                res.send(resData);
+                break;
+
+            default:
+                console.log("Unknown intent", req.body.intent.name);
+        }
     } else if (req.body.request.type === "SessionEndedRequest") {
-        onSessionEnded(req.body.request, req.body.session);
-        res.send();
+        var resData = buildResponse(req.body.session.attributes, "End request called")
+        res.send(resData);
     }
     // res.send(buildSpeechletResponse("Hello"));
     // res.send(buildSpeechletResponse("Hi, I'm hotel booking bot, I can book hotel for you in any city of the world"));
@@ -59,8 +117,7 @@ function onIntent(intentRequest, session, callback) {
     console.log("onIntent requestId=" + intentRequest.requestId
         + ", sessionId=" + session.sessionId);
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
+    var intent = intentRequest.intent, intentName = intentRequest.intent.name;
 
     // dispatch custom intents to handlers here
     if (intentName == 'TestIntent') {
@@ -75,63 +132,63 @@ function onIntent(intentRequest, session, callback) {
  * Called when the user ends the session.
  * Is not called when the skill returns shouldEndSession=true.
  */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+// function onSessionEnded(sessionEndedRequest, session) {
+//     console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+//         + ", sessionId=" + session.sessionId);
 
-    // Add any cleanup logic here
-}
-function handleTestRequest(intent, session, callback) {
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Hello, World!", "", "true"));
-}
+//     // Add any cleanup logic here
+// }
+// function handleTestRequest(intent, session, callback) {
+//     callback(session.attributes,
+//         buildSpeechletResponseWithoutCard("Hello, World!", "", "true"));
+// }
 
 // ------- Helper functions to build responses -------
 
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
+// function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+//     return {
+//         outputSpeech: {
+//             type: "PlainText",
+//             text: output
+//         },
+//         card: {
+//             type: "Simple",
+//             title: title,
+//             content: output
+//         },
+//         reprompt: {
+//             outputSpeech: {
+//                 type: "PlainText",
+//                 text: repromptText
+//             }
+//         },
+//         shouldEndSession: shouldEndSession
+//     };
+// }
 
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
+// function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+//     return {
+//         outputSpeech: {
+//             type: "PlainText",
+//             text: output
+//         },
+//         reprompt: {
+//             outputSpeech: {
+//                 type: "PlainText",
+//                 text: repromptText
+//             }
+//         },
+//         shouldEndSession: shouldEndSession
+//     };
+// }
 
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}
+// function buildResponse(sessionAttributes, speechletResponse) {
+//     return {
+//         version: "1.0",
+//         sessionAttributes: sessionAttributes,
+//         response: speechletResponse
+//     };
+// }
 
 
 
